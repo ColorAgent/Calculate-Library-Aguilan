c9fa90e89b92f01440d609593f56b0caf49b5e05
APCS3rd <APCS3rd@MHS-Dreyer-01>
2018-09-11 11:08:15 -0700

    Changes 1.0

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,15 @@
+/* This class is client code that uses the Calculate library to perform various mathematical task.
+ * @author Alex Aguilan
+ * Version September 6th
+ */
+public class DoMath {
+	
+	 public static void main(String[] args) {
+		 System.out.println(Calculate.square(5)); 
+		 System.out.println(Calculate.cube(7));
+		 System.out.println(Calculate.average(7.35, 7.65));
+		 System.out.println(Calculate.average(5.5, 5.5, 21.4));
+		 System.out.println(Calculate.toDegrees);
+	}
+	 
+}
