152107e1c88ee1d6d1a5cf2fbf5038d7afdca647
Alex Aguilan <42448363+ColorAgent@users.noreply.github.com>
2018-09-23 21:37:13 -0700

    Update DoMath.java

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -24,7 +24,7 @@ public class DoMath {
 		 System.out.println(Calculate.exponent(5, 4.7));
 		 System.out.println(Calculate.factorial(8));
 		 System.out.println(Calculate.isPrime(9));
-         System.out.println(Calculate.gcf(28,8));
+		 System.out.println(Calculate.gcf(28,8));
 		 
 	}
 	 
