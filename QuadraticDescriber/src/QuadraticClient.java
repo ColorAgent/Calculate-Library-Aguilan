82a441aa5af86d40c60e119f2c9cbc61e7f74608
APCS3rd <APCS3rd@MHS-Dreyer-01>
2018-10-09 12:16:56 -0700

    2.0 idk

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
+/* This class is client code that uses the Quadratic Library(Quadratic library) to perform various mathematical task.
+ * @author Alex Aguilan 
+ * Version October 6th
+ */
 public class QuadraticClient {
+	Scanner Console = new Scanner(System.in);
+	// This ask for user input before progressing
 	public static String quadDescriber (double a, double b, double c) {
-		return null;
+		double fas = console.nextInt();
+		double b = console.nextInt();
+		double c = console.nextInt();
+		System.out.println(Quadratic.quadForm(a, b, c));
 	}
 }
