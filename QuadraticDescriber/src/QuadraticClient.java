1a3da0bcf0e8600a5bfa08707a6be4395478a8ef
Alex Aguilan <Alex_Aguilan@users.noreply.github.com>
2018-10-11 00:51:00 -0700

    WIP 2.0

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,4 +1,16 @@
+/*\Takes the input from the user and outputs the description of the graph
+ * @author Alex Aguilan
+ * @version 5 October 2018
+ */
 
+import java.util.*;
+
+//ask users for coefficients of a quadratic equation & prints description of the graph
 public class QuadraticClient {
-	public static String quadDescriber (double a, double b, double c)
-}
+	public static void main(String[] args) {
+		Scanner Input = new Scanner(System.in);
+		boolean keepgoing = true;
+		System.out.println("Welcome to Quadratic Describer" + "\nThis will provide values for the coefficients a, b, and c!");
+		
+		//ask user for variables and print description of graph
+		while(keepgoing){
